dc161680c80153762049f8592eef8d8415e6a479
Andrew Davie <andrew@taswegian.com>
2026-05-18 02:19:10 +1000

    removed crud

--- main/savekey.h
+++ main/savekey.h
@@ -5,27 +5,6 @@
 
 #include <stdbool.h>
 
-#define SK_SIZE ((100 + 7) / 8)
-
-extern unsigned char saveKeyUnlocked[SK_SIZE];
-extern unsigned char saveKeySolved[SK_SIZE];
-extern unsigned char saveKeyPerfect[SK_SIZE];
 extern unsigned char saveKeyEnableICC;
-
-void setUnlockStatus(int lot);
-bool getUnlockStatus(int lot);
-bool getSolveStatus(int lot);
-bool getPerfectStatus(int lot);
-void setPerfectStatus(int lot);
-bool isGameCompleted();
-bool unlockLevels();
-bool isGroupSolved(int lot);
-bool isGroupPerfect(int lot);
-// bool isPriorSolved(int lot);
-// bool isPriorPerfect(int lot);
-void setSolveStatus(int lot);
-
-// extern bool priorGroupSolved[];
-// extern bool priorGroupPerfect[];
 
 // EOF
