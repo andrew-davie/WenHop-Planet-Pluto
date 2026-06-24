c35bc3e0074079db11b1e255225f6f3d5105d5d6
Andrew Davie <andrew@taswegian.com>
2026-06-25 02:15:54 +1000

    planet info work PYTHON DASM!!!!!

---
+++ gfx/tools/make.sh
@@ -0,0 +1,3 @@
+python3 ./alphanumeric.py --output alphanumeric ../alphanumeric/alphanumeric.png
+python3 ./alphanumeric.py --output fontcompact ../alphanumeric/fontCompact.png
+python3 ./alphanumeric.py --output fontlarge ../alphanumeric/fontLarge.png
